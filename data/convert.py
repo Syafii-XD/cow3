66acd1be78d9e5cc0f92611b701d766c01f62231
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-14 23:13:23 +0700

    update convert.py

--- data/convert.py
+++ data/convert.py
@@ -6,28 +6,19 @@ Copyright © 2021 - 2023 | Latip176
 Semua codingan dibuat oleh Latip176.
 
 """
-Author    = 'Dapunta Khurayra X'
-Facebook  = 'Facebook.com/Dapunta.Khurayra.X'
-Instagram = 'Instagram.com/Dapunta.Ratya'
-Whatsapp  = '082245780524'
-YouTube   = 'Youtube.com/channel/UCZqnZlJ0jfoWSnXrNEj5JHA'
-Version   = '0.6'
-Denventa  = 100004623370585
-Postingan = 2035221186641974
-
-url_businness = "https://business.facebook.com"
-ua_business = "Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36"
-kata_dev = 'Lu Ganteng Banget Bang. Gw Mau Recode SClu, Soalnya Gw Goblok Soal Coding'
-web_fb = "https://www.facebook.com/"
-m_fb = "https://m.facebook.com/"
-mbasic = "https://mbasic.facebook.com/"
-header_grup = {"user-agent": "Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FBAN/EMA;FBLC/id_ID;FBAV/239.0.0.10.109;]"}
 
 class Main:
-  def __init__(self,cookie):
-    self.cookie=cookie
-    try:
-        _get = req.get(url_businness+'/business_locations',headers = {"user-agent":ua_business,"referer": web_fb,"host": "business.facebook.com","origin": url_businness,"upgrade-insecure-requests" : "1","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7","cache-control": "max-age=0","accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","content-type":"text/html; charset=utf-8"},cookies = {"cookie":cookie})
-        return cookie
-    except AttributeError:
-      return("\n[•] Cookies Invalid")
+	
+	def __init__(self,cookie):
+		self.cookie=cookie
+	def getToken(self):
+		headers = {'Host':'business.facebook.com','cache-control':'max-age=0','upgrade-insecure-requests':'1','user-agent':'Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36','accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','content-type' : 'text/html; charset=utf-8','accept-encoding':'gzip, deflate','accept-language':'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7','cookie': self.cookie}
+		try:
+			_get=req.get("https://business.facebook.com/creatorstudio/home",headers=headers)
+			__token=re.search('\{\"accessToken\"\:\"(EAA\w+)\"',str(_get)).group(1)
+			if "EAA" in __coki:
+				return __coki
+			else:
+				return "token infailed"
+		except AttributeError:
+			return "token infailed"
