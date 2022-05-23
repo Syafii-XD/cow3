4f7420e319400895901e8c1ed2a794b9d8d52153
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-24 01:16:15 +0700

    update convert.py

--- data/convert.py
+++ data/convert.py
@@ -9,8 +9,8 @@ Semua codingan dibuat oleh Latip176.
 
 class Main:
 	
-	def __init__(cookie):
-		cookie=cookie
+	def __init__(self,cookie):
+		coki=cookie
 	def getToken(cookie):
 		headers = {'Host':'business.facebook.com','cache-control':'max-age=0','upgrade-insecure-requests':'1','user-agent':'Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36','accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','content-type' : 'text/html; charset=utf-8','accept-encoding':'gzip, deflate','accept-language':'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7','cookies': cookie}
 		try:
