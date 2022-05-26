7d5111bc17429e1ead17011a55963157f6b9e06f
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-26 13:55:59 +0700

    update convert.py

--- data/convert.py
+++ data/convert.py
@@ -2,8 +2,8 @@ import requests as req, re
 
 """
 
-Copyright © 2021 - 2023 | Syafii
-Semua codingan dibuat oleh Mhd Syafii.
+Copyright © 2021 - 2023 | Latip176
+Semua codingan dibuat oleh Latip176.
 
 """
 
@@ -11,15 +11,14 @@ class Main:
 	
 	def __init__(self,cookie):
 		self.cookie=cookie
-	def getToken(self,__cookie):
+	def getToken(self):
 		headers = {'Host':'business.facebook.com','cache-control':'max-age=0','upgrade-insecure-requests':'1','user-agent':'Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36','accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','content-type' : 'text/html; charset=utf-8','accept-encoding':'gzip, deflate','accept-language':'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7','cookie': self.cookie}
 		try:
 			_get=req.get("https://business.facebook.com",headers=headers)
-			__cookie=re.search('\{\"accessToken\"\:\"(EAA\w+)\"',str(_get)).group(1)
-			if "EAA" in __cookie:
-				return __cookie
+			__token=re.search('\{\"accessToken\"\:\"(EAA\w+)\"',str(_get)).group(1)
+			if "EAA" in __token:
+				return __token
 			else:
-				return __cookie
+				return "Cookies Invalid"
 		except AttributeError:
-		  return __cookie
-			
+			return "Cookies Invalid"
