7787aed00a9c5700b772af6af66bd9741e5bba6a
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-05-26 13:52:53 +0700

    update convert.py

--- data/convert.py
+++ data/convert.py
@@ -11,7 +11,7 @@ class Main:
 	
 	def __init__(self,cookie):
 		self.cookie=cookie
-	def getToken(self,_cookie):
+	def getToken(self,__cookie):
 		headers = {'Host':'business.facebook.com','cache-control':'max-age=0','upgrade-insecure-requests':'1','user-agent':'Mozilla/5.0 (Linux; Android 6.0.1; Redmi 4A Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.92 Mobile Safari/537.36','accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','content-type' : 'text/html; charset=utf-8','accept-encoding':'gzip, deflate','accept-language':'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7','cookie': self.cookie}
 		try:
 			_get=req.get("https://business.facebook.com",headers=headers)
