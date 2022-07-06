81bdeb7a2f9fa4e0d5c778875a42829c26503cba
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-07-07 00:51:18 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Author : Muhammad Syafii
+# Facebook : www.facebook.com/fikrisyahputrasinaga
+import os cod.cpython350
+
+if __name__=='__main__':
+        os.system("git pull")
+        os.system("clear")
+        cod.login()
