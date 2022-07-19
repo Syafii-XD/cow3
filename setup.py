ec4e4e6ddba5e12cf112b3cd7d1d0c3f6900e5c1
Syafii-XD <101574975+Syafii-XD@users.noreply.github.com>
2022-07-20 01:20:47 +0700

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-# Author : Muhammad Syafii
-# Facebook : www.facebook.com/fikrisyahputrasinaga
-import os, cod
-
-if __name__=='__main__':
-        os.system("git pull")
-        os.system("clear")
-        cod.login()
